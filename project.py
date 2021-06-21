c3018ee044d93825dd49803a68cadde8ca780ef0
vivekanandjadhav <85631563+vivekanandjadhav@users.noreply.github.com>
2021-06-21 19:09:11 +0530

    this is only for the demo purpose

---
+++ project.py
@@ -0,0 +1,17 @@
+v=int(input(5))
+lis=[2,3,6,6,5]
+#for i in range(v):
+   #   x=int(input())
+    #  lis.append(x)
+#print(lis)
+print("hey")
+lis.sorted()
+#print(max(lis))
+#a.reversed()
+print("hey")
+i=len(lis)-1
+for i in range(len(lis),lis[0],-1):
+    if len[lis]!=lis[i]:
+        print("hey")
+        print("the max output is",len[lis],lis[i])
+        break
